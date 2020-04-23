e684d01955b3f0f269d57f1147bc144428a4dc68
Sacha Sedriks <sachased@gmail.com>
2020-04-22 22:15:09 -0700

    added new event listener so when a change is detected in the 'sort-type' it makes another fetch call to yelp.api. Btw, renamed anything with 'search-criteria' to 'sort-type'.

--- js/master.js
+++ js/master.js
@@ -13,6 +13,7 @@
 // INIT
 function init() {
   handleSubmission();
+  handleSort();
  }
 
 $(() => {
@@ -39,7 +40,7 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function fetchRestaurantInfo(area, distance, diet, criteria = 'best_match') {
+function fetchRestaurantInfo(area, distance, diet, sort = 'best_match') {
  
   const baseURL = 'https://api.yelp.com/v3/businesses/search';
   const apiKey = 'IGnYDkKpA5hFg8el7-9WyyoLx5Z5sv2nssKYPflu_KGq26puqqFYSR9vikWHbTeSt9Vm1xzlQYKjzvf7uoJrkNTNfGdgJ5S7H3OW_CXlTJChkm-HxwgWNFnx-fOZXnYx';
@@ -66,7 +67,7 @@ function fetchRestaurantInfo(area, distance, diet, criteria = 'best_match') {
     location: area,
     categories: diet,
     radius: distanceMeters,
-    sort_by: criteria
+    sort_by: sort
   };
 
   // add sorting
@@ -75,8 +76,6 @@ function fetchRestaurantInfo(area, distance, diet, criteria = 'best_match') {
   const queryString = formatQueryParams(params);
   const url = proxyBypassURL + baseURL + '?' + queryString;
   console.log(url);
-
-
 
   fetch(url, options)
     .then(response => {
@@ -88,6 +87,7 @@ function fetchRestaurantInfo(area, distance, diet, criteria = 'best_match') {
     .then(data => {
       console.log(data);
       renderSearchResults(data);
+      $('.please-wait').text('');
     })
     .catch(err => console.log(err));
   }  
@@ -155,8 +155,8 @@ function handleSubmission() {
     const area = $('.area-input').val();
     $('.view-results').find('.area-input').val(area);
     const distance = $('.distance-input').val();
-    const criteria = $('.search-criteria').val();
-    console.log(criteria);
+    const sort = $('.sort-type').val();
+    // console.log(sort);
     const diet = [];
     if ($('#gluten-free-check').is(':checked')) {
       diet.push('gluten_free');
@@ -170,9 +170,38 @@ function handleSubmission() {
       diet.push('vegetarian');
       $('.view-results').find('#vegetarian-check').attr('checked', true);
     } 
-    fetchRestaurantInfo(area, distance, diet, criteria);
+    fetchRestaurantInfo(area, distance, diet, sort);
   });
 }
+
+// ** REFACTOR **
+// as this fn is nearly identical to handleSubmission fn
+// (with just different event listeners)
+// should refactor so it's more efficient between
+// the two fn without duplication of code
+function handleSort() {
+  $('.sort-type').on('change', event => {
+    const sort = $(event.target).val();
+    // console.log($(event.target).val());
+    const area = $('.area-input').val();
+    const distance = $('.distance-input').val();
+    const diet = [];
+    if ($('#gluten-free-check').is(':checked')) {
+      diet.push('gluten_free');
+      $('.view-results').find('#gluten-free-check').attr('checked', true);
+    } 
+    if ($('#vegan-check').is(':checked')) {
+      diet.push('vegan');
+      $('.view-results').find('#vegan-check').attr('checked', true);
+    } 
+    if ($('#vegetarian-check').is(':checked')) {
+      diet.push('vegetarian');
+      $('.view-results').find('#vegetarian-check').attr('checked', true);
+    } 
+    $('.please-wait').text('Please wait while we re-sort the results...');
+    fetchRestaurantInfo(area, distance, diet, sort);
+  });
+} 
 
 
 
